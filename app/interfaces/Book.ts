c064768cda7dd56458798d774c9a8834fe4a562b
Ethan Green <66663131+edg35@users.noreply.github.com>
2026-01-17 10:44:20 -0500

    feat: reading section addition

---
+++ app/interfaces/Book.ts
@@ -0,0 +1,18 @@
+export default interface Book {
+    title: string;
+    author: string;
+    cover: string;
+    coverMedium: string;
+    coverSmall: string;
+    bookId: string;
+    isbn: string;
+    description: string;
+    publishedYear: string;
+    averageRating: string;
+    userRating: string;
+    userShelves: string;
+    userReadAt: string;
+    userDateAdded: string;
+    numPages: string;
+    link: string;
+}
